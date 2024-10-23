f7f15f7f6dab3f2e5414abe56c97649b6e2400cf
huzaifa-bilal-01 <huzaifabilal226@gmail.com>
2024-10-23 15:49:12 +0500

    add: accesstoken verification

--- dist/index.js
+++ dist/index.js
@@ -9,17 +9,19 @@ const io = new socket_io_1.Server(server, {
         methods: ["GET", "POST"]
     }
 });
+const PORT = process.env.PORT || 4000;
 let masterTabId = null;
 io.on('connection', (socket) => {
-    // console.log('New client connected');
     socket.on('REGISTER_MASTER', (data) => {
         masterTabId = data.tabId;
-        console.log(`Master tab registered: ${masterTabId}`);
+        // console.log(`Master tab registered: ${data.accessToken}`);
     });
     socket.on('MOUSE_POSITION', (data) => {
+        console.log("Mouse data: ", data);
         socket.broadcast.emit('MOUSE_POSITION_UPDATE', {
             position: data.position,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
     socket.on('SCROLL_POSITION', (data) => {
@@ -27,86 +29,95 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('SCROLL_POSITION_UPDATE', {
             position: data.position,
             masterTabId: data.masterTabId,
-            timestamp: data.timestamp
+            timestamp: data.timestamp,
+            accessToken: data.accessToken
         });
     });
     socket.on('CLICK_EVENT', (data) => {
-        // console.log("click: ",data)
         socket.broadcast.emit('CLICK_EVENT_UPDATE', {
             clickData: data.clickData,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
     socket.on('CONTEXT_MENU_EVENT', (data) => {
         socket.broadcast.emit('CONTEXT_MENU_EVENT_UPDATE', {
             contextMenuData: data.contextMenuData,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
     socket.on('INPUT_CHANGE', (data) => {
-        // console.log("Input: ",data)
         socket.broadcast.emit('INPUT_CHANGE_UPDATE', {
             inputData: data.inputData,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
     socket.on('MOUSE_DOWN', (data) => {
         socket.broadcast.emit('MOUSE_DOWN_UPDATE', {
             mouseDownData: data.mouseDownData,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
     socket.on('MOUSE_UP', (data) => {
         socket.broadcast.emit('MOUSE_UP_UPDATE', {
             mouseUpData: data.mouseUpData,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
     socket.on('DOUBLE_CLICK', (data) => {
         socket.broadcast.emit('DOUBLE_CLICK_UPDATE', {
             doubleClickData: data.doubleClickData,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
     socket.on('WHEEL_ZOOM', (data) => {
-        // console.log("Wheel: ", data);
         socket.broadcast.emit('WHEEL_ZOOM_UPDATE', {
             zoomData: data.zoomData,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
     socket.on('DRAG_MOVE', (data) => {
-        // console.log("DRAG MOVE: ",data)
         socket.broadcast.emit('DRAG_MOVE_UPDATE', {
             dragMoveData: data.dragMoveData,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
     socket.on('DRAG_START', (data) => {
         socket.broadcast.emit('DRAG_START_UPDATE', {
             dragStartData: data.dragStartData,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
     socket.on('DRAG_END', (data) => {
         socket.broadcast.emit('DRAG_END_UPDATE', {
             dragEndData: data.dragEndData,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
     socket.on('MOUSE_OVER', (data) => {
         socket.broadcast.emit('MOUSE_OVER_UPDATE', {
             mouseOverData: data.mouseOverData,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
     socket.on('MOUSE_OUT', (data) => {
         socket.broadcast.emit('MOUSE_OUT_UPDATE', {
             mouseOutData: data.mouseOutData,
-            masterTabId: data.masterTabId
+            masterTabId: data.masterTabId,
+            accessToken: data.accessToken
         });
     });
 });
-server.listen(4000, () => {
-    console.log('WebSocket server listening on port 4000');
+server.listen(PORT, () => {
+    console.log(`WebSocket server listening on port ${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,23 @@ const io = new Server(server, {
     methods: ["GET", "POST"]
   }
 });
+const PORT = process.env.PORT || 4000;
 
 let masterTabId: number | null = null;
 
 io.on('connection', (socket) => {
-  // console.log('New client connected');
 
   socket.on('REGISTER_MASTER', (data) => {
     masterTabId = data.tabId;
-    console.log(`Master tab registered: ${masterTabId}`);
+    // console.log(`Master tab registered: ${data.accessToken}`);
   });
 
   socket.on('MOUSE_POSITION', (data) => {
+    // console.log("Mouse data: ", data)
     socket.broadcast.emit('MOUSE_POSITION_UPDATE', {
       position: data.position,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
 
@@ -31,101 +33,111 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('SCROLL_POSITION_UPDATE', {
       position: data.position,
       masterTabId: data.masterTabId,
-      timestamp: data.timestamp
+      timestamp: data.timestamp,
+      accessToken: data.accessToken
     });
   });
 
   socket.on('CLICK_EVENT', (data) => {
-    // console.log("click: ",data)
     socket.broadcast.emit('CLICK_EVENT_UPDATE', {
       clickData: data.clickData,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
 
   socket.on('CONTEXT_MENU_EVENT', (data) => {
     socket.broadcast.emit('CONTEXT_MENU_EVENT_UPDATE', {
       contextMenuData: data.contextMenuData,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
 
   socket.on('INPUT_CHANGE', (data) => {
-    // console.log("Input: ",data)
     socket.broadcast.emit('INPUT_CHANGE_UPDATE', {
       inputData: data.inputData,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
 
   socket.on('MOUSE_DOWN', (data) => {
     socket.broadcast.emit('MOUSE_DOWN_UPDATE', {
       mouseDownData: data.mouseDownData,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
 
   socket.on('MOUSE_UP', (data) => {
     socket.broadcast.emit('MOUSE_UP_UPDATE', {
       mouseUpData: data.mouseUpData,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
 
   socket.on('DOUBLE_CLICK', (data) => {
     socket.broadcast.emit('DOUBLE_CLICK_UPDATE', {
       doubleClickData: data.doubleClickData,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
 
   socket.on('WHEEL_ZOOM', (data) => {
-    // console.log("Wheel: ", data);
     socket.broadcast.emit('WHEEL_ZOOM_UPDATE', {
       zoomData: data.zoomData,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
 
   socket.on('DRAG_MOVE', (data) => {
-  // console.log("DRAG MOVE: ",data)
     socket.broadcast.emit('DRAG_MOVE_UPDATE', {
       dragMoveData: data.dragMoveData,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
 
   socket.on('DRAG_START', (data) => {
     socket.broadcast.emit('DRAG_START_UPDATE', {
       dragStartData: data.dragStartData,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
 
   socket.on('DRAG_END', (data) => {
-  
+
     socket.broadcast.emit('DRAG_END_UPDATE', {
       dragEndData: data.dragEndData,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
 
   socket.on('MOUSE_OVER', (data) => {
     socket.broadcast.emit('MOUSE_OVER_UPDATE', {
       mouseOverData: data.mouseOverData,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
 
   socket.on('MOUSE_OUT', (data) => {
     socket.broadcast.emit('MOUSE_OUT_UPDATE', {
       mouseOutData: data.mouseOutData,
-      masterTabId: data.masterTabId
+      masterTabId: data.masterTabId,
+      accessToken: data.accessToken
     });
   });
-  
+
 });
 
-server.listen(4000, () => {
-  console.log('WebSocket server listening on port 4000');
+server.listen(PORT, () => {
+  console.log(`WebSocket server listening on port ${PORT}`);
 });
+
